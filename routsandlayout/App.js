ec586bfc5b609e232d5a68813b241710b694cade
raghavendraraghu16 <raghavendra672006@gmail.com>
2025-11-18 15:46:17 +0530

    Update App.js

--- routsandlayout/App.js
+++ routsandlayout/App.js
@@ -2,22 +2,9 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  const userProfile = {
-    name: "Niranjan",
-    age: 25,
-    email: "niranjan@example.com",
-    location: "Srirangapatna, India",
-    interests: ["fitness", "coding", "traveling"]
-  };
-
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>User Profile</Text>
-      <Text>Name: {userProfile.name}</Text>
-      <Text>Age: {userProfile.age}</Text>
-      <Text>Email: {userProfile.email}</Text>
-      <Text>Location: {userProfile.location}</Text>
-      <Text>Interests: {userProfile.interests.join(", ")}</Text>
+      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -27,13 +14,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'flex-start',
+    alignItems: 'center',
     justifyContent: 'center',
-    padding: 20
   },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 10
-  }
 });
