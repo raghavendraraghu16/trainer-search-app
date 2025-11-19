ab526e60888448aa8e2e3e7558a151fbaa6b7396
raghavendraraghu16 <raghavendra672006@gmail.com>
2025-11-19 12:12:50 +0530

    Test
    app

---
+++ Test/app/_layout.jsx
@@ -0,0 +1,38 @@
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { Tabs } from "expo-router";
+
+export default function TabLayout() {
+  return (
+    <Tabs screenOptions={{ tabBarActiveTintColor: "blue" }}>
+      <Tabs.Screen
+        name="home"
+        options={{
+          title: "home",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome size={28} name="home" color={color} />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="project"
+        options={{
+          title: "project",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome size={28} name="cog" color={color} />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="contact"
+        options={{
+          title: "contact",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome size={28} name="cog" color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
